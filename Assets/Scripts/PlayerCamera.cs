cc37448d0da3a9980f27ba33719da8d1b21701d4
onedanijo11@gmail.com <onedanijo11@gmail.comy>
2024-09-03 22:42:13 +0900

    added controls+camera

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private GameObject player;
+    private Vector3 offset;
+    public float mouseSensitivity = 100.0f;
+    private float pitch = 30.0f;
+    private float yaw = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = GameObject.Find("SpherePlayer");
+        if (this.player == null)
+        {
+            Debug.LogError("Player object not found!");
+            return;
+        }
+        offset = transform.position - this.player.transform.position;
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the center of the screen
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.player != null)
+        {
+            // Get mouse input
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            // Update yaw and pitch
+            yaw += mouseX;
+            pitch -= mouseY;
+            pitch = Mathf.Clamp(pitch, -90f, 90f); // Limit pitch to avoid flipping
+
+            // Calculate new camera position
+            Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
+            Vector3 newPosition = this.player.transform.position + rotation * offset;
+
+            // Update camera position and rotation
+            transform.position = newPosition;
+            transform.LookAt(this.player.transform.position);
+        }
+    }
+
+    // Draw camera direction arrow in the scene view
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * 200);
+        if (this.player != null)
+        {
+            //Playmode中のみ実行
+        }
+    }
+
+    public Vector3 GetForwardDirection()
+    {
+        return transform.forward;
+    }
+}
--- Assets/Scripts/SpherePlayer.cs
+++ Assets/Scripts/SpherePlayer.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class SpherePlayer : MonoBehaviour
 {
+    Rigidbody rb;
+
+    float force = 100.0f;
+    float torque = 10.0f;
+    PlayerCamera playerCamera;
+    Vector3 cameraForward;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject cameraObject = GameObject.Find("PlayerCamera"); 
+        if (cameraObject != null)
+        {
+            playerCamera = cameraObject.GetComponent<PlayerCamera>();
+        }
+        rb = this.GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 100.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+    }
+
+    void FixedUpdate() //物理演算関連はできるだけこちらで処理
+    {
+        //Platyerカメラから進行方向を取得
+        if (playerCamera != null)
+        {
+            cameraForward = playerCamera.GetForwardDirection();
+        }
+
+        //WASDで前後左右に力を加える
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(force * cameraForward);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-force * cameraForward);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.Cross(cameraForward, Vector3.up) * force);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(-Vector3.Cross(cameraForward, Vector3.up) * force);
+        }
+
+        //Spaceでジャンプ
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            rb.AddForce(Vector3.up * force*10, ForceMode.Impulse);
+        }
+
+        //Shiftでトルクを加える
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.AddTorque(-Vector3.Cross(cameraForward, Vector3.up)*torque, ForceMode.Impulse);
+        }
     }
 }
